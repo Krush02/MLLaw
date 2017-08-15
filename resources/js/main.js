705a2b8ae0e4dd3ad5623043a2ab49b46d802f54
Jason Patrick <jasonpatrick02@me.com>
2017-08-15 11:16:20 -0400

    intial commit
    MLLaw site - initial commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,180 @@
+// main function >> nav menu toggle, FA hover effects, smooth scroll
 var main = function() {
-  $(document).click( function(){
-    $('.dropdown-menu').hide();
+
+  // hide dropdown-menu when 'click' on document
+  //$(document).click( function(){
+  //  $('.dropdown-menu').hide();
+  //});
+
+  // stop hide() from bubbling & open dropdown-menu when bars are clicked
+  //$('.fa-bars').click(function() {
+  //  event.stopPropagation();
+  //  $('.dropdown-menu').slideToggle(200, 'swing');
+  //});
+
+  //Slow Yo Scroll, Homie
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          });
+        }
+      }
+    });
+
+    //Banner auto-type effect
+  var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+  };
+
+  // blurb highlight hover effect
+  $('.blurb').hover(function() {
+    $(this).addClass('highlight');
+  }, function() {
+    $(this).removeClass('highlight');
   });
 
-  $('.fa-bars').click(function() {
-    event.stopPropagation();
-    $('.dropdown-menu').slideToggle(200, "swing");
+  // gavel spin on hover effect
+  $('#gavel').hover(function() {
+    $('#gavel i').addClass("fa-rotate-270");
+  }, function() {
+    $('#gavel i').removeClass("fa-rotate-270");
   });
-}
 
+  // hourglass spin on hover effect
+  $('#hourglass').hover(function() {
+    $('#hourglass i').addClass("fa-rotate-270");
+  }, function() {
+    $('#hourglass i').removeClass("fa-rotate-270");
+  });
+
+  // globe spin on hover effect
+  $('#globe').hover(function() {
+    $('#globe i').addClass("fa-rotate-270");
+  }, function() {
+    $('#globe i').removeClass("fa-rotate-270");
+  });
+};
+
+$('.btn-family').click(function() {
+  $('#modal-one').fadeIn();
+});
+
+$('.btn-business').click(function() {
+  $('#modal-two').fadeIn();
+});
+
+$('.btn-real').click(function() {
+  $('#modal-three').fadeIn();
+});
+
+$('.btn-commercial').click(function() {
+  $('#modal-four').fadeIn();
+});
+
+$('.btn-civil').click(function() {
+  $('#modal-five').fadeIn();
+});
+
+$('.btn-consumer').click(function() {
+  $('#modal-six').fadeIn();
+});
+
+$('.btn-estate').click(function() {
+  $('#modal-seven').fadeIn();
+});
+
+$('.btn-probate').click(function() {
+  $('#modal-eight').fadeIn();
+});
+
+$('.btn-employment').click(function() {
+  $('#modal-nine').fadeIn();
+});
+
+$('.closer').click(function() {
+  $('.modal').fadeOut();
+});
+
+//call main function when document is ready
 $(document).ready(main);
