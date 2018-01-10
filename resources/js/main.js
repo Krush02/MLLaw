0a5c1ef22b1f8b511dad6b39d4caaacf58160bfc
Jason Patrick <jasonpatrick02@me.com>
2018-01-10 11:49:13 -0500

    add floating back to top button

--- resources/js/main.js
+++ resources/js/main.js
@@ -207,5 +207,19 @@ var main = function() {
   	//$(this).toggleClass('invert');
   //});
 
+  // ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
 //call main function when document is ready
 $(document).ready(main);
