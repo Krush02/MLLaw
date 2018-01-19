1ffad2b589980e1670a23167f6541c5427f573f8
Jason Patrick <jasonpatrick02@me.com>
2018-01-19 10:49:27 -0500

    revamp smooth scrolling - simple/concise javascript, & fix focus issue

--- resources/js/main.js
+++ resources/js/main.js
@@ -13,41 +13,20 @@ var main = function() {
   });
 
   //Slow Yo Scroll, Homie
-  // Select all links with hashes
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-        location.hostname == this.hostname
-      ) {
-        // Find element to scroll to
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
         var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            }
-          });
-        }
-      }
-    });
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+    }
+  });
 
     //Banner auto-type effect
   var TxtRotate = function(el, toRotate, period) {
@@ -166,7 +145,7 @@ $(window).scroll(function() {
 $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
-    }, 500);
+    }, 1000);
 });
 
 //call main function when document is ready
