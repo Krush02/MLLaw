496d98b07e4a60e39fc1a30bf4b7b4e56c3e6542
Jason Patrick <jasonpatrick02@me.com>
2018-01-19 11:03:12 -0500

    indentation

--- resources/js/main.js
+++ resources/js/main.js
@@ -97,18 +97,18 @@ var main = function() {
   });
 
   // ===== Scroll to Top ====
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 200) {       // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 1000);
-});
+  $(window).scroll(function() {
+      if ($(this).scrollTop() >= 200) {       // If page is scrolled more than 50px
+          $('#return-to-top').fadeIn(200);    // Fade in the arrow
+      } else {
+          $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+      }
+  });
+  $('#return-to-top').click(function() {      // When arrow is clicked
+      $('body,html').animate({
+          scrollTop : 0                       // Scroll to top of body
+      }, 1000);
+  });
 
 //call main function when document is ready
 $(document).ready(main);
